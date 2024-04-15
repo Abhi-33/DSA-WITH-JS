2cfcebd03f459989c1ea33fdcdaccb4b15304536
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-04-15 16:21:01 +0530

    added two inital examples

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //Add- Examples
 //compare the two solutions to find the sum of numbers from 1 to n.
 
-function addUpton(n){ //solution1
-    let total = 0;
-    for(let i = 0;i<=n;i++){
-        total+=i;
-    }
-    return total;
-}
-console.log(addUpton(5));
+// function addUpton(n){ //solution1
+//     let total = 0;
+//     for(let i = 0;i<=n;i++){
+//         total+=i;
+//     }
+//     return total;
+// }
+// console.log(addUpton(5));
 
 //solution2 
 function addUpton(n){
