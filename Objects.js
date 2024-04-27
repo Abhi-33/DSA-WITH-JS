3e5512c9a878bae6b21f638f58c427cc90c75934
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-04-27 15:36:41 +0530

    Updated

---
+++ Objects.js
@@ -0,0 +1,11 @@
+// Objects - unordered key value pairs 
+
+// When to Use Objects : - 1) When you don't need order.
+                        // 2) When you need fast access/insertion and removal.
+
+// Example 1
+let instructor = {
+    firstName : "kelly",
+    isInstructor : true,
+    favoriteNumbers : [1,2,3,4]
+}
