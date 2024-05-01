25c7de0bbcef93b7a2a5f36e989a4a128699ac35
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-05-01 09:10:09 +0530

    Updated

---
+++ ProblemSolving_strategies.js
@@ -0,0 +1,15 @@
+//Problem Solving Strategies 
+//1) UNDERSTAND THE PROBLEM 
+
+//PROBLEM : Write a function which takes two numbers and returns their sum.
+
+//1. Can i restate the problem in my own words ?
+      "implement addition"
+//2. What are the inputs that go into the problem ?
+    //  -ints?
+    //  -floats?
+    //  - what about string for large numbers ? 
+//3. What are the outputs that should come from the solution to the problem ?
+        // -ints? floatS? strings?
+//4. Can the outputs be determined from the inputs ? (do i have enough information to solve the problem)
+//5. How should i label the pieces of data that are a part of the problem ?
